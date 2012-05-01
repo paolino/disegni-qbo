75945ea045d11c5bdb56ce65b1716ff386cdf082
Paolo Veronelli <paolo.veronelli@gmail.com>
2012-05-01 23:08:23 +0200

    first

--- PannelloSolare.hs
+++ PannelloSolare.hs
@@ -9,9 +9,11 @@ import GancioPannello
 larghezza = 100.5
 spessore = 1
 
+distanza_verticale_slitte = 76
+spiazzamento_verticale_slitte_alte = 20
 ascissa_slitta = 18
-ordinata_alta_slitte = larghezza - 20
-ordinata_bassa_slitte = ordinata_alta_slitte - 76
+ordinata_alta_slitte = larghezza - spiazzamento_verticale_slitte_alte
+ordinata_bassa_slitte = ordinata_alta_slitte - distanza_verticale_slitte
 
 pannello = cubo larghezza spessore larghezza 
 
